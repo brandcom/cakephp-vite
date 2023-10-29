4b71f3645fe17b3761bb5c58837c632b14abd173
passchn <77938819+passchn@users.noreply.github.com>
2023-10-29 19:18:56 +0100

    auto load plugin manifest

--- src/Exception/ViteHelperException.php
+++ src/Exception/ViteHelperException.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace ViteHelper\Exception;
 
-abstract class ViteHelperException extends \Exception
+use Exception;
+
+abstract class ViteHelperException extends Exception
 {
 }
