2814e1fabb00b829cbc14796bdf0471a8e11914e
Attila Bőr <bor.attila89@gmail.com>
2024-03-20 22:56:26 +0000

    Updated default config file

--- config/app_vite.php
+++ config/app_vite.php
@@ -3,13 +3,13 @@
 return [
 	'ViteHelper' => [
 		'plugin' => false,
-		'environment' => \ViteHelper\Enum\Environment::FROM_DETECTOR,
+		'environment' => \ViteHelper\Enum\Environment::PRODUCTION,
+		'build' => [
+			'outDirectory' => 'build',
+			'manifest' => WWW_ROOT . 'build' . DS . '.vite' . DS . 'manifest.json',
+		],
 		'development' => [
 			'url' => 'http://localhost:3000',
-		],
-		'build' => [
-			'outDirectory' => false,
-			'manifest' => WWW_ROOT . 'manifest.json',
 		],
 	],
 ];
